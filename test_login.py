2c7132b1fecdbf5f64baf5b6146b1a0dd3273cf0
minhcode-dev <lamminh395@gmail.com>
2024-12-16 11:03:45 +0700

    Update test_login.py - Minh

--- test_login.py
+++ test_login.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import login as l
 @pytest.fixture
 def driver():
     driver = webdriver.Edge()
@@ -14,23 +14,13 @@ def driver():
 
 #login successfully
 def test_login(driver):
-    driver.get("http://localhost/ProjectWeb/index.php")
-    driver.find_element(By.XPATH,"//*[@id='user-img']").click()
-    driver.find_element(By.XPATH,"//*[@id='user-btn']").click()
-    driver.find_element(By.XPATH,"//*[@id='username']").send_keys("tester111")
-    driver.find_element(By.XPATH,"//*[@id='password']").send_keys("123456789")
-    driver.find_element(By.CLASS_NAME,"submit-btn").click()
+    l.login(driver)
     time.sleep(5)
     assert driver.current_url=="http://localhost/ProjectWeb/index.php","Failed to login"
 
-#login wrong password or blank
+#login wrong password
 def test_login_wrong_password(driver):
-    driver.get("http://localhost/ProjectWeb/index.php")
-    driver.find_element(By.XPATH,"//*[@id='user-img']").click()
-    driver.find_element(By.XPATH,"//*[@id='user-btn']").click()
-    driver.find_element(By.XPATH,"//*[@id='username']").send_keys("tester111")
-    driver.find_element(By.XPATH,"//*[@id='password']").send_keys("123")
-    driver.find_element(By.CLASS_NAME,"submit-btn").click()
+    l.login_wrong_pass(driver)
     try:
         error_msg = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, "//*[@id='error-msg']"))
@@ -39,14 +29,29 @@ def test_login_wrong_password(driver):
     except Exception as e:
         assert False, f"Error message not found or didn't appear in time: {str(e)}"
 
-#login wrong username or blank
+def test_login_blank_pass(driver):
+    l.login_wrong_pass(driver)
+    try:
+        error_msg = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//*[@id='error-msg']"))
+        ).text
+        assert error_msg == "Sai tên đăng nhập hoặc mật khẩu", f"Expected 'Sai tên đăng nhập hoặc mật khẩu', but got '{error_msg}'"
+    except Exception as e:
+        assert False, f"Error message not found or didn't appear in time: {str(e)}"
+
+#login wrong username
 def test_login_wrong_username(driver):
-    driver.get("http://localhost/ProjectWeb/index.php")
-    driver.find_element(By.XPATH,"//*[@id='user-img']").click()
-    driver.find_element(By.XPATH,"//*[@id='user-btn']").click()
-    driver.find_element(By.XPATH,"//*[@id='username']").send_keys("tester11")
-    driver.find_element(By.XPATH,"//*[@id='password']").send_keys("123456789")
-    driver.find_element(By.CLASS_NAME,"submit-btn").click()
+    l.login_wrong_user(driver)
+    try:
+        error_msg = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//*[@id='error-msg']"))
+        ).text
+        assert error_msg == "Sai tên đăng nhập hoặc mật khẩu", f"Expected 'Sai tên đăng nhập hoặc mật khẩu', but got '{error_msg}'"
+    except Exception as e:
+        assert False, f"Error message not found or didn't appear in time: {str(e)}"
+
+def test_login_blank_username(driver):
+    l.login_wrong_user(driver)
     try:
         error_msg = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, "//*[@id='error-msg']"))
@@ -57,12 +62,7 @@ def test_login_wrong_username(driver):
 
 #login blank all 
 def test_login_blank_all(driver):
-    driver.get("http://localhost/ProjectWeb/index.php")
-    driver.find_element(By.XPATH,"//*[@id='user-img']").click()
-    driver.find_element(By.XPATH,"//*[@id='user-btn']").click()
-    driver.find_element(By.XPATH,"//*[@id='username']").send_keys("")
-    driver.find_element(By.XPATH,"//*[@id='password']").send_keys("")
-    driver.find_element(By.CLASS_NAME,"submit-btn").click()
+    l.login_blank_all(driver)
     try:
         error_msg = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, "//*[@id='error-msg']"))
